f12536a254b27bb24008514ae32df643e6c41685
gabriel <devmonca@gmail.com>
2025-02-05 21:28:58 -0300

    Project created

---
+++ src/main/java/com/devmonca/smlist/SmlistApplication.java
@@ -0,0 +1,13 @@
+package com.devmonca.smlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmonca/smlist/SmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmonca.smlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
